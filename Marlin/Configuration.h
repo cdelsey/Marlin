5b010d82213bbbdc5284e41959a60b74d77444cc
Delsey <carl.r.delsey@intel.com>
2020-03-13 09:20:21 -0700

    Swapping bed dimensions

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -891,8 +891,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 520
-#define Y_BED_SIZE 465
+#define X_BED_SIZE 465 
+#define Y_BED_SIZE 520
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
